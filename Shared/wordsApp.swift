653ec655e149f64746c03d9d1279f0da2c40e7c3
Dan Weiner <dan@danielweiner.org>
2021-04-10 14:37:25 -0400

    Initial Commit

---
+++ Shared/wordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordsApp.swift
+//  Shared
+//
+//  Created by Dan Weiner on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct wordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
